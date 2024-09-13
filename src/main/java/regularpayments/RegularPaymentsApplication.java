be370d5c973b5ee0d5edbe325a766c7a782aae75
Dima Morsch <149238209+Dmytro-Morsch@users.noreply.github.com>
2024-09-13 15:39:42 +0300

    Initial commit

---
+++ src/main/java/regularpayments/RegularPaymentsApplication.java
@@ -0,0 +1,13 @@
+package regularpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegularPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegularPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/regularpayments/RegularPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package regularpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegularPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
